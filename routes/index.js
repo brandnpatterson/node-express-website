6852552ae2d8cf5c721f280ad67d0bdd32595d73
Brandon Patterson <brandnpatterson@outlook.com>
2020-04-14 09:59:52 -0500

    use routes dir

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const router = require('express').Router();
-const { home, auth, messages, profile } = require('../controllers');
+const { Post } = require('../models');
 
-router.get('/', home);
-router.get('/login', auth);
-router.get('/messages', messages);
-router.get('/profile', profile);
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.findAll();
+
+    res.render('home', { posts });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get('/auth', (req, res) => {
+  res.render('auth');
+});
+
+router.get('/messages', (req, res) => {
+  res.render('messages');
+});
+
+router.get('/profile', (req, res) => {
+  res.render('profile');
+});
 
 module.exports = router;
